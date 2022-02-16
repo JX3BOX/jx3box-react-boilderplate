76b01479b9b173d860732daf56e04d07c9c0d9d6
Harper Gao <harper.gao@ringcentral.com>
2022-02-16 22:10:48 +0800

    [JBB-4] Add var.less, less demo

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,20 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `$injectedColor: orange;`,
+      },
+      less: {
+        globalVars: {
+          hack: `true; 
+              @import "./node_modules/csslab/base.less";
+              @import "./node_modules/@jx3box/jx3box-common/css/var.less";
+              @import "./src/assets/less/var.less";`,
+        },
+      },
+    },
+  },
   plugins: [react()],
 });
