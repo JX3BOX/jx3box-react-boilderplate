b36878ba5c47468e492e7774b0bccd4cf2a906d8
Harper Gao <harper.gao@ringcentral.com>
2022-02-19 21:58:12 +0800

    [JBB-18] Add vite production config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"),
+    },
+  },
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
   },
