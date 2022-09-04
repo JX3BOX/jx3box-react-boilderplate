0f1db40b0ab49a6389259ab19ca4f6707fe968a1
Harper Gao <harper.gao@ringcentral.com>
2022-09-04 11:47:36 +0800

    Skip lib check and add global.d.ts

---
+++ src/types/global.d.ts
@@ -0,0 +1,10 @@
+declare module "@jx3box/jx3box-common/js/request" {
+  export function $cms(): any;
+  export function $next(): any;
+  export function $helper(): any;
+  export function $node(): any;
+  export function $team(): any;
+  export function $pay(): any;
+  export function $lua(): any;
+  export function $http(): any;
+}
