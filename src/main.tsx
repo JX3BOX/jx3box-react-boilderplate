a42b3e8c7873bc7d7c47ac0028a447fa206da1c0
Harper Gao <harper.gao@ringcentral.com>
2022-09-04 11:55:07 +0800

    Update react version to 18.x

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import Jx3App from "./jx3-app";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <Jx3App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
